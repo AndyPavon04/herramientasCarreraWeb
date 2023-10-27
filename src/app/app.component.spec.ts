284c59ad8202812bd99df861dc08e24293054322
AndyPavon04 <andy.perezp@alumno.buap.mx>
2023-10-27 11:57:47 -0600

    Login y registro

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,6 +14,12 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
+  it('should create the app', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.componentInstance;
+    expect(app).toBeTruthy();
+  });
+
   it(`should have as title 'carrera-webapp-hw'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
