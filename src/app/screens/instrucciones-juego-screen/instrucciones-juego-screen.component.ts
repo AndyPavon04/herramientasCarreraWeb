166d2b5a4dd4b9b95b2ba67a0e489ac430ab3ab7
AndyPavon04 <andy.perezp@alumno.buap.mx>
2023-11-27 17:28:48 -0600

    Se agregaron las instrucciones de juego

--- src/app/screens/instrucciones-juego-screen/instrucciones-juego-screen.component.ts
+++ src/app/screens/instrucciones-juego-screen/instrucciones-juego-screen.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-instrucciones-juego-screen',
@@ -6,5 +7,50 @@ import { Component } from '@angular/core';
   styleUrls: ['./instrucciones-juego-screen.component.scss']
 })
 export class InstruccionesJuegoScreenComponent {
+  public lista_codigos: any[] = [];
 
+  constructor(
+    private router: Router,
+  ) { }
+
+  ngOnInit(): void {
+    this.obtenerCodigos();
+  }
+
+  public isMobile(){
+    if(/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini|Mobile|mobile|CriOS/i.test(navigator.userAgent)){
+      return "interior-mobile";
+    }else{
+      return "interior-normal";
+    }
+  }
+
+  public obtenerCodigos(){
+    this.lista_codigos = [
+      {
+        "accion": "codigo-redimido",
+        "puntos": 1000,
+      },
+      {
+        "accion": "Jugar sin chocar de 0 a 30 segundos",
+        "puntos": 100,
+      },
+      {
+        "accion": "Jugar sin chocar de 31 a 60 segundos",
+        "puntos": 200,
+      },
+      {
+        "accion": "Jugar sin chocar de 61 a 90 segundos",
+        "puntos": 300,
+      },
+      {
+        "accion": "Jugar sin chocar de 91 a 120 segundos",
+        "puntos": 400,
+      }
+    ];
+  }
+
+  public jugarAhora(){
+    this.router.navigate(["juego"])
+  }
 }
